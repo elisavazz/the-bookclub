0f6057bab7af3d652d14723cb38c18854df4f63f
vazz <elisa.merinovazquez@gmail.com>
2018-09-19 21:16:07 +0200

    first commit, new schemes

---
+++ src/server/models/Book.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+	title: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	author: {
+		type: String,
+		required: true
+	},
+	bookCover: {
+		type: String,
+		default:
+			'https://vignette.wikia.nocookie.net/2007scape/images/7/7a/Mage%27s_book_detail.png/revision/latest?cb=20180310083825'
+	},
+	language: {
+		type: String,
+		required: true
+	},
+	extimatedReadingDays: {
+		type: Number,
+		required: true
+	},
+	availability: Boolean,
+	isbn: String
+});
+
+module.exports = mongoose.model('Book', bookSchema);
--- src/server/routes/api/auth.js
+++ src/server/routes/api/auth.js
@@ -1,57 +1,70 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../../models/User')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const config = require('../../config')
-const upload = require('../../utils/upload')
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
+const upload = require('../../utils/upload');
+const saltRounds = 10;
+var salt = bcrypt.genSaltSync(saltRounds);
 
 router.post('/sign-up', (req, res) => {
-    const { email, password } = req.body
+	const { email, password, language } = req.body;
+	console.log(password);
+	if (!email || !password || !language) res.status(400).send({ error: 'Missing Credentials.' });
 
-    if (!email || !password) res.status(400).send({ error: 'Missing Credentials.' })
+	User.findOne({ email })
+		.then((existingUser) => {
+			if (existingUser) return res.status(400).send({ error: 'E-Mail exists already.' });
 
-    User.findOne({ email })
-        .then(existingUser => {
-            if (existingUser) return res.status(400).send({ error: 'E-Mail exists already.' })
-
-            return req.files && req.files.picture ? upload(req.files.picture) : Promise.resolve()
-        })
-        .then(pictureUrl => {
-            const hashedPassword = bcrypt.hashSync(password, 10)
-            return new User({ email, password: hashedPassword, profilePicture: pictureUrl }).save()
-        })
-        .then(user => {
-            const token = jwt.sign(
-                { _id: user._id, email: user.email, profilePicture: user.profilePicture },
-                config.SECRET_JWT_PASSPHRASE
-            )
-            res.send({ token })
-        })
-})
+			return req.files && req.files.picture ? upload(req.files.picture) : Promise.resolve();
+		})
+		.then((pictureUrl) => {
+			const hashedPassword = password;
+			//bcrypt.hashSync(password, salt);
+			return new User({
+				email,
+				password: hashedPassword,
+				profilePicture: pictureUrl,
+				language
+			}).save();
+		})
+		.then((user) => {
+			const token = jwt.sign(
+				{
+					_id: user._id,
+					email: user.email,
+					profilePicture: user.profilePicture,
+					language: user.language
+				},
+				config.SECRET_JWT_PASSPHRASE
+			);
+			res.send('fooo'); //change to { token }
+		});
+});
 
 router.post('/sign-in', (req, res) => {
-    const { email, password } = req.body
+	const { email, password } = req.body;
 
-    if (!email || !password) res.status(400).send({ error: 'Missing Credentials.' })
+	if (!email || !password) res.status(400).send({ error: 'Missing Credentials.' });
 
-    User.findOne({ email }).then(existingUser => {
-        if (!existingUser) return res.status(400).send({ error: 'User does not exist.' })
+	User.findOne({ email }).then((existingUser) => {
+		if (!existingUser) return res.status(400).send({ error: 'User does not exist.' });
 
-        const passwordsMatch = bcrypt.compareSync(password, existingUser.password)
+		const passwordsMatch = bcrypt.compareSync(password, existingUser.password);
 
-        if (!passwordsMatch) return res.status(400).send({ error: 'Password is incorrect.' })
+		if (!passwordsMatch) return res.status(400).send({ error: 'Password is incorrect.' });
 
-        const token = jwt.sign(
-            {
-                _id: existingUser._id,
-                email: existingUser.email,
-                profilePicture: existingUser.profilePicture,
-            },
-            config.SECRET_JWT_PASSPHRASE
-        )
-        res.send({ token })
-    })
-})
+		const token = jwt.sign(
+			{
+				_id: existingUser._id,
+				email: existingUser.email,
+				profilePicture: existingUser.profilePicture
+			},
+			config.SECRET_JWT_PASSPHRASE
+		);
+		res.send({ token });
+	});
+});
 
-module.exports = router
+module.exports = router;
