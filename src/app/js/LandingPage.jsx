e0d8629e8b437d53f1360e18ea091a3304e76e19
vazz <elisa.merinovazquez@gmail.com>
2018-09-27 22:19:56 +0200

    ALMOST DONE

--- src/app/js/LandingPage.jsx
+++ src/app/js/LandingPage.jsx
@@ -30,7 +30,10 @@ const LandingPage = (props) => {
 					<div className="container landing-container wrap-container">
 						<div className="wow content-right">
 							<ScrollAnimation animateIn="fadeIn" className="error-msg">
-								<h2 className="titling">A very good book is like a good friend</h2>
+								<h2 className="titling">
+									A very good book is like a{' '}
+									<span className="underlined-text">good friend</span>
+								</h2>
 								<br />
 								<p className="subtitle book-title">
 									Meeting the readers community is easy: sign up, update your location,
