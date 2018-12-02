33a6d100632c5506354b7731f6916efb8767370f
elisavazz <elisa.merinovazquez@gmail.com>
2018-12-02 20:54:17 +0100

    'heroku'

--- src/server/config.js
+++ src/server/config.js
@@ -1,11 +1,12 @@
 
+require("dotenv").config();
 module.exports = {
 	IS_PRODUCTION: process.env.NODE_ENV === 'production',
 	PORT: process.env.PORT || 3000 ,
 	MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/the-bookclub',
-	SECRET_JWT_PASSPHRASE: "TWLom9RDbmGYBtkHHPe4m8pKswyUY",
-	CLOUDINARY_NAME: "vazzz",
-	CLOUDINARY_KEY: "964288365416837",
-	CLOUDINARY_SECRET: "Y8TAPydKy-b2iWDQkdBWEH5TNVY",
-	ZIPCODE_API: "zRY3rJBOJiW4eE5Hwn7HmHeIWd4i4x5VxDF00OVFIMOUHpXCshh5UGAFeYmfnNFV"
+	SECRET_JWT_PASSPHRASE: process.env.SECRET_JWT_PASSPHRASE,
+	CLOUDINARY_NAME:process.env.CLOUDINARY_NAME,
+	CLOUDINARY_KEY: process.env.CLOUDINARY_KEY,
+	CLOUDINARY_SECRET: process.env.CLOUDINARY_SECRET,
+	ZIPCODE_API: process.env.ZIPCODE_API
 };
